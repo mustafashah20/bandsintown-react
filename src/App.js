642d1122bbe1b1abf8697654e370686a2dad3ed2
Mustafa Shah <i170253@nu.edu.pk>
2021-06-24 19:00:19 +0500

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Search from './components/search'
 
 function App() {
   return (
     <div className="App">
       <main>
         <section className="glass">
-          
+          <Search/>
         </section>
       </main>
     </div>
---
+++ src/components/search.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Search = () => {
+    return (
+        <p className="display1">Search</p>
+    );
+}
+ 
+export default Search;
