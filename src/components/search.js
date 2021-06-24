a0607be4ee4751128b5067db6332d3952a07bcaf
Mustafa Shah <i170253@nu.edu.pk>
2021-06-25 03:04:13 +0500

    fetch artist complete

--- src/components/search.js
+++ src/components/search.js
@@ -2,12 +2,69 @@ import React, { useState } from 'react'
 import { BsSearch } from "react-icons/bs";
 
 const Search = () => {
+
+    const [inputVal, setInputVal] = useState('')
+    const [name, setName] = useState('')
+    const [eventCount, setEventCount] = useState('')
+
+    const handleInputChange = (e) => {
+        setInputVal(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        if(e.keyCode === 13){
+            getArtist()
+        }
+    }
+
+    //`https://rest.bandsintown.com/artists/${ inputVal }?app_id=12345`
+
+    //`http://localhost:3005/artist?name=${inputVal}`
+
+    //`https://www.bandsintown.com/searchSuggestions?searchTerm=${inputVal}`
+
+    //`http://localhost:3005/search?name=${inputVal}`
+
+    const getArtist = () => {
+        try {
+            fetch(`https://rest.bandsintown.com/artists/${inputVal}?app_id=12345`).then(
+            (response) => response.json().then(
+              (data) => {
+                console.log(data)
+                setName(data.name)
+                setEventCount(data.upcoming_event_count)
+              }
+            )
+          )
+          } catch (error) {
+            console.log(error)
+          }
+    }
+    
+
     return (  
-        <div class="input-group shadow-sm mt-5">
-            <input type="text" class="form-control my-input" placeholder="Search Artist" aria-label="artist" aria-describedby="basic-addon1"/>
-            <span class="input-group-text my-input-icon" id="basic-addon1">
-                <BsSearch class="my-icon"/>
-            </span>
+        <div>
+            <div className="input-group shadow-sm mt-5">
+                <input type="text" 
+                    className="form-control my-input" 
+                    placeholder="Search Artist" 
+                    aria-label="artist" 
+                    aria-describedby="basic-addon1"
+                    onChange={handleInputChange}
+                    onKeyDown={handleSubmit}
+                    />
+
+                <span className="input-group-text my-input-icon" id="basic-addon1">
+                    <BsSearch className="my-icon"/>
+                </span>
+                
+            </div>
+            {
+                name!=='' && <p className="display-5">Artist: {name}</p>
+            }
+            {
+                eventCount!=='' &&  <p className="display-5">Event Count: {eventCount}</p>
+            }
         </div>
     );
 }
