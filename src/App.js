875c1a06fc719038b6856c08c6b0810152cab870
Mustafa Shah <i170253@nu.edu.pk>
2021-06-24 18:48:50 +0500

    background added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My App</h1>
-      <p>This is my bands in town application!</p>
+      <main>
+        <section className="glass">
+          
+        </section>
+      </main>
     </div>
   );
 }
